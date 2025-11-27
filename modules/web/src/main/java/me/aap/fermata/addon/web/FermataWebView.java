403e3c02d2bbb3af180f43fdc03b7af9281a98d0
HoangBac1209 <HoangBac1209@GMAIL.COM>
2025-11-27 16:47:04 +0700

    Update FermataWebView.java
    fix run background with media service

--- modules/web/src/main/java/me/aap/fermata/addon/web/FermataWebView.java
+++ modules/web/src/main/java/me/aap/fermata/addon/web/FermataWebView.java
@@ -101,6 +101,8 @@ public class FermataWebView extends WebView
 
 		setDesktopMode(addon, false);
 		setForceDark(addon, false);
+		getSettings().setUserAgentString("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/21.0 Chrome/110.0.5481.154 Safari/537.36"); //fix run background 
+
 	}
 
 	@Override
